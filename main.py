832f4423955b72a6a44d0c9b04e36a63c3f01df7
SOL <16487848-SOL-ADALYADALY@users.noreply.replit.com>
2024-03-15 13:17:20 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,26 @@
+#Sol pulido
+#09/21/23
+
+#created a variables that let's the user input information and then output it
+
+print("Let's play Silly Sentences! ")
+print(" ")
+name=input("Enter a name: ")
+adjective1=input("Enter an adjective: ")
+adjective2=input("Enter an adjective: ")
+adverb=input("Enter an adverb: ")
+food1=input("Enter a food: ")
+food2=input("Enter another food: ")
+noun=input("Enter a noun: ")
+place=input("Enter a place: ")
+verb=input("Enter a verb: ")
+#using print() i output the information that the user input in 
+print(" ")
+print(name+ " was planning a dream vacation to " + place + ".")
+print(name+ " was especially looking forward to trying the local")
+print("cuisine, including " + adjective1 +" "+ food1 + " and " + food2 + ".")
+print(" ")
+print(name+ " will have to practice the language " + adverb + " to") 
+print("make it easier to " + verb + " with people.")
+print(name+ " has a long list of sights to see, including the")
+print( noun + " museum and the " + adjective2 + " park.")
